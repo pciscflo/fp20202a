4a4f624cdb83ef37d8b7ac42694c4c8ebb5e32a0
escorpio80@gmx.com <escorpio80@gmx.com>
2020-10-31 07:07:10 -0500

    31.10.1

---
+++ app/src/test/java/com/upc/crudproductos/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.crudproductos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
