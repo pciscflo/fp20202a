55fa8f7d3d2c48ce5ce1b1685ec0993012132996
escorpio80@gmx.com <escorpio80@gmx.com>
2020-10-31 09:22:06 -0500

    12.10.3

---
+++ Semana4/src/com/upc/curso/App2.java
@@ -0,0 +1,12 @@
+package com.upc.curso;
+
+public class App2 {
+    public static void main(String[] args) {
+        boolean valor=true;
+        if (valor)  // es lo mismo que if (valor==true)
+            System.out.println("ES verdadero");
+        else
+            System.out.println("Es falso");
+        System.out.println("Fin del programa");
+    }
+}
